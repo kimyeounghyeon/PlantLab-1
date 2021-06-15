e24bba8b52f38c17cc2ff2c8dfc138830df73ea8
Baeyr <ripel10188@gmail.com>
2021-06-15 09:51:05 +0900

    DB 데이터타입 변경에따른 상품vo 변경

--- src/main/java/com/plant/lab/product/model/vo/Product.java
+++ src/main/java/com/plant/lab/product/model/vo/Product.java
@@ -7,11 +7,12 @@ public class Product {
 	private int pro_no;
 	private String pro_name;
 	private String pro_image;
-	private int pro_price;
+	private String pro_price;
 	private String pro_content;
 	private int pro_totalStar;
 	private int pro_stock;
 	private char pro_cate;
+	
 	
 	public int getPro_no() {
 		return pro_no;
@@ -31,10 +32,10 @@ public class Product {
 	public void setPro_image(String pro_image) {
 		this.pro_image = pro_image;
 	}
-	public int getPro_price() {
+	public String getPro_price() {
 		return pro_price;
 	}
-	public void setPro_price(int pro_price) {
+	public void setPro_price(String pro_price) {
 		this.pro_price = pro_price;
 	}
 	public String getPro_content() {
@@ -49,7 +50,6 @@ public class Product {
 	public void setPro_totalStar(int pro_totalStar) {
 		this.pro_totalStar = pro_totalStar;
 	}
-	
 	public int getPro_stock() {
 		return pro_stock;
 	}
@@ -62,6 +62,4 @@ public class Product {
 	public void setPro_cate(char pro_cate) {
 		this.pro_cate = pro_cate;
 	}
-	
-	
 }
